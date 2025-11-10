6a9f163ff91d2559a6dba18f12006da59bc351a9
MarollaVictor <marollavictorsedky@gmail.com>
2025-11-10 18:28:08 +0200

    Adds Tpay digest config and SourceLink support
    Introduces a new digest entry for Tpay integration in configuration to enable secure communication.
    
    Generates SourceLink mapping for improved debugging and traceability with repository sources.
    
    Updates assembly informational version for build reproducibility.

--- obj/Debug/net9.0/tpay_integration_task.AssemblyInfo.cs
+++ obj/Debug/net9.0/tpay_integration_task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpay_integration_task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7211afe97dd571e874b47049514cd725b4746194")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpay_integration_task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpay_integration_task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
